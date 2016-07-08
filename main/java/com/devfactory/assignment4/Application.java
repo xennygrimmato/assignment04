c2862cf3b5d823fae0be59882893245339170153
Vaibhav Tulsyan <vstulsyan@gmail.com>
2016-07-08 22:00:58 +0400

    assignment04a

---
+++ main/java/com/devfactory/assignment4/Application.java
@@ -0,0 +1,15 @@
+package com.devfactory.assignment4;
+
+/**
+ * Created by vaibhavtulsyan on 07/07/16.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
